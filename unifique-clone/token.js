40a89c2aa10a9256b5f9e1b5d7082bbe9f446136
kaua.silveira1 <kauadasilvasilveira14@gmail.com>
2022-11-10 13:54:20 -0300

    ok

---
+++ unifique-clone/token.js
@@ -0,0 +1,7 @@
+function getUrlBase(port) {
+    return "https://ufibihsqdddxwnrxkccu.supabase.co/rest/v1/" + port + "select=*";
+}
+
+function getTokenSupabase() {
+    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmaWJpaHNxZGRkeHducnhrY2N1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjMyNjI3NDIsImV4cCI6MTk3ODgzODc0Mn0.0Qf0Jpl8L4be-6ybsjL3YIhdDJQuNPcMvXC7PIQa424";
+}
